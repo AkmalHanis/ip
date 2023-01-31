2846da05ebb8d8efa1289d2182d4041847bc80d8
Akmal_Hanis <akmalightning@gmail.com>
2023-02-01 04:17:44 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,9 +6,12 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
+        String[] list;
+        list = new String[101];
 
         int repeat = 1;
         Scanner enter = new Scanner(System.in);
+        int counter = 0;
         while (repeat == 1) {
 
             String echo = enter.nextLine();
@@ -16,10 +19,21 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else {
+            }
+            if (echo.equals("list")){
+                System.out.println("Your saved task:");
+                for (int i = 0; i < counter; i += 1) {
+                    if (list[i].equals("")){
+                        break;
+                    }
+                        System.out.println((i + 1) + ". " + list[i]);
+                    }
                 System.out.println("____________________________________________________________");
-                System.out.println(echo);
-                System.out.println("____________________________________________________________");
+
+            }
+            else {
+                list[counter] = echo;
+                counter += 1;
             }
         }
     }
