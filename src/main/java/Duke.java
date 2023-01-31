fc453d27e741a691c93239db7ab628e6abf0a73b
Akmal_Hanis <akmalightning@gmail.com>
2023-02-01 03:55:16 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 public class Duke {
+
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+
+        int repeat = 1;
+        Scanner enter = new Scanner(System.in);
+        while (repeat == 1) {
+
+            String echo = enter.nextLine();
+            if (echo.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(echo);
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
